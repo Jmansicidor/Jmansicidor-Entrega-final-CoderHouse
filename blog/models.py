7176f43ea8c5a0abe87a4ca26121cf8f600d612a
John Cotamo <johncotamo@hotmail.com>
2022-06-17 06:05:25 -0500

    se realiza asociacion de user en blog y en usuarios

--- blog/models.py
+++ blog/models.py
@@ -1,17 +1,18 @@
 from django.db import models
 
-
 # Create your models here.
+from usuarios.models import UserInfo
+
 
 class Avatar(models.Model):
     pass
 
 
 class Create(models.Model):
-    user = models.CharField(max_length=200)
+    user = models.ForeignKey(UserInfo, on_delete=models.SET_NULL, null=True)
     theme = models.CharField(max_length=100)
     date = models.DateField(auto_now=True)
     text_area = models.TextField(max_length=500)
 
     def __str__(self):
-        return f"{self.user} {self.theme} {self.date} {self.text_area}  "
+        return f"{self.user.name} {self.theme} {self.date} {self.text_area}  "
