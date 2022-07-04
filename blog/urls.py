1b417f98f06156d0accd738e4470e651f9ec4fd1
John Alexander Cotamo <johncotamo@hotmail.com>
2022-07-04 10:16:17 -0500

    path de Profile
    se añade para poder  visualizar desde el navegador

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from blog import views
 
 urlpatterns = [
     path('blog_new/', views.blog_new.as_view(), name="blogcrear"),
+    path('blog_profile/', views.blog_profile.as_view(), name="blogprofile"),
     path('', views.blog_list.as_view(), name="bloginicio"),
     path('blog_detail/<pk>/', views.blog_detail.as_view(), name="blogdetalle"),
     path('blog_edit/<pk>/', views.blog_edit.as_view(), name="blog_update"),
